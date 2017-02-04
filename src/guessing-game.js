50979105f25c822469adf671a321b8a0a2ba666c
Julia Golovach <jenmorgan93@gmail.com>
2017-02-04 16:35:31 +0300

    added some code

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,41 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min=min;
+        this.max=max;
+        this.mid=Math.floor((this.min+this.max)/2);
+    }
 
     setRange(min, max) {
-
+        min=this.min;
+        max=this.max;
+        this.answer=Math.floor(Math.random()*(max-min+1)+min);
     }
 
     guess() {
-
+        var guessed = this.mid;
+        if (guessed===this.answer){
+            return this.answer;
+        }
+        else if (this.answer<guessed){
+            this.lower();
+        }
+        else if (this.answer>guessed) {
+            this.greater();
+        }
     }
 
     lower() {
-
+        var min=this.mid;
+        var max=this.max;
+        this.answer=Math.floor(Math.random()*(max-min+1)+min);
+        this.guess();
     }
 
     greater() {
-
+        var max=this.mid;
+        var min=this.min;
+        this.answer=Math.floor(Math.random()*(max-min+1)+min);
+        this.guess();
     }
 }
 
